6a8511ad60b596b3a20222cb547d1ab9caa0f745
Henry Morales <limik@gmx.us>
2022-11-15 18:21:08 -0500

    Re-commit
    Something went wrong with the last commit, and library.cpp never got updated properly.  This is the updated version.

--- src/library/library.cpp
+++ src/library/library.cpp
@@ -14,13 +14,14 @@
 #include <ctype.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netdb.h>
 #include "library.h"
 
-SlangLib::SlangLib(char c, int p, string h){
+SlangLib::SlangLib(char c){
     switch(c){
         case 's':
         case 'S':
@@ -34,62 +35,60 @@ SlangLib::SlangLib(char c, int p, string h){
             cout << "Error: Invalid Connection Type" << endl;
             exit(EXIT_FAILURE);
     }
-    portNumber = p;
-	hostname = h;
 }
 
-void SlangLib :: wordleRead(int sock){
-    char *receiveHello;
-    int receiving = recv(sock,receiveHello, strlen(receiveHello),0);
-    if(receiving == -1){
-        perror("Error receiving Message");
-        exit(EXIT_FAILURE); // exit on failure of receiving message
-    }
-
+string SlangLib :: wordleRead(int sock){
+    char *recvChar = (char *)malloc(10);
+	read(sock, recvChar, 9);
+	string recv(recvChar);
+	free(recvChar);
+	return recv;
 }
 
-void SlangLib :: wordleWrite(int sock){
-    string sayHello = "HELLO";
-    int sending = send(sock,sayHello.c_str(),sayHello.length(),0);
+void SlangLib :: wordleWrite(int sock, string message){
+    int sending = write(sock, message.c_str(), message.length());
     if(sending == -1){
         perror("Error Sending Message");
-        exit(EXIT_FAILURE); // exit on failure of sendng message
     }
 }
 
-void SlangLib :: connection(){
+void SlangLib :: init(string h, int p){
+	hostname = h;
+	portNumber = p;
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock == -1){
         perror("Socket Error\n");
-        exit(EXIT_FAILURE); //exit upon failure of socket creation
+        exit(EXIT_FAILURE);
     }
     if(connectionType == 'S'){
-		struct sockaddr_in baseConnection = {AF_INET, portNumber, INADDR_ANY};
-
-		/*static struct sigaction act;
-		act.sa_handler = catcher;
-		sigfillset(&(act.sa_mask));
-		sigaction(SIGPIPE, &act, NULL);*/
-
+		struct sockaddr_in baseConnection, activeConnection;
+		memset(&baseConnection, 0, sizeof(struct sockaddr_in));
+		baseConnection.sin_family = AF_INET;
+		baseConnection.sin_addr.s_addr = htonl(INADDR_ANY);
+		baseConnection.sin_port = htons(portNumber);
         int binding = bind(sock, (struct sockaddr *)&baseConnection, sizeof(struct sockaddr));
         if(binding == -1){
             perror("Error in Bind Call:\n");
-            exit(EXIT_FAILURE); // exit on failure of bind call
+            exit(EXIT_FAILURE);
         }
-        int listening = listen(sock, 2);
+        int listening = listen(sock, 5);
         if(listening == -1){
             perror("Error in Listen Call:\n");
-            exit(EXIT_FAILURE); // exit on failure of listen call
+            exit(EXIT_FAILURE);
         }
-		socklen_t socklen = sizeof(struct sockaddr);
+		socklen_t infolen = sizeof(activeConnection);
         while(true){
-            int accepting = accept(sock,(struct sockaddr *)&baseConnection, &socklen);
-            if(accepting == -1){
+            int accepting = accept(sock, (struct sockaddr *)&activeConnection, &infolen);
+            if(accepting < 0){
                 perror("Error in Accepting Connections:\n");
-                exit(EXIT_FAILURE); //exit on failure of listen call
+                exit(EXIT_FAILURE);
             }
-			wordleWrite(sock);
-			wordleRead(sock);
+			cout << "SERVER SEND: 5(HELLO)\n";
+			sleep(1);
+			wordleWrite(accepting, "5(HELLO)");
+			string word = wordleRead(accepting);
+			cout << "SERVER RECV: " << word << endl;
+			close(accepting);
         }
     }
     else{
@@ -104,7 +103,7 @@ void SlangLib :: connection(){
 			printf("Error in Calling getaddrinfo: %s\n", gai_strerror(getConnectionInfo));
 			freeaddrinfo(hints);
 			freeaddrinfo(baseConnection);
-			exit(EXIT_FAILURE); // exit on failure of getaddrinfo
+			exit(EXIT_FAILURE);
 		}
 		((struct sockaddr_in *)baseConnection->ai_addr)->sin_port = htons(portNumber);
 		int connection = connect(sock, (struct sockaddr *)baseConnection->ai_addr,sizeof(struct sockaddr));
@@ -112,18 +111,24 @@ void SlangLib :: connection(){
 			perror("Connection Error\n:");
 			freeaddrinfo(hints);
 			freeaddrinfo(baseConnection);
-			exit(EXIT_FAILURE); /// exit  on failure of connection call
+			exit(EXIT_FAILURE);
 		}
-        wordleWrite(sock);
-        wordleRead(sock);
+		string word = wordleRead(sock);
+		cout << "CLIENT RECV: " << word << endl;
+		sleep(1);
+		cout << "CLIENT SEND: 4(QUIT)\n";
+		sleep(1);
+		wordleWrite(sock, "4(QUIT)");
+		close(sock);
 		freeaddrinfo(hints);
 		freeaddrinfo(baseConnection);
+		sleep(1);
     }
 }
 
 int SlangLib :: errorChecking(int recvCheck,int connectCheck, int sockCheck){
     if(recvCheck == -1){
-        perror("Error in receiving message\n");
+        perror("Error in recieving message\n");
         return -1;
     } // check when call recv
 
