8941b803868f7c4fd6e878dd0748ba860141d9c9
NewModelM <82418078+NewModelM@users.noreply.github.com>
2022-11-26 18:40:34 -0500

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 #include <iostream>
+#include <random>
 #include <fstream>
-#include <unordered_map>
+#include <vector>
 #include "libslang.h"
 using namespace std;
 
-
+/*
 void readIntoMap(unordered_map<string, bool>& stringHash) {
 
 
@@ -24,16 +25,47 @@ void readIntoMap(unordered_map<string, bool>& stringHash) {
 
 
 }
+*/
 
+int readIntoVector(vector<string>& stringVector) {
+
+
+        ifstream inFile;
+        string fileName = "words.txt";
+
+
+        string word;
+        int length = 0;
+        while(inFile >> word) {
+
+
+                stringVector.push_back(word);
+                length++;
+        }
+
+
+        return length;
+}
+
+
+int randomNumGen(int maxInt) {
+
+
+        default_random_engine gen;
+        uniform_int_distribution<int> dist(1, maxInt);
+        return dist(gen);
+}
 
 int main() {
 
 
-	unordered_map<string, bool> stringHash;
-	readIntoMap(stringHash);
+	//unordered_map<string, bool> stringHash;
+	//readIntoMap(stringHash);
+        vector<string> stringVector;
+        int fileLength = readIntoVector(stringVector);
 
 
-
+        const string correctAns = stringVector[randomNumGen(fileLength)];
 
 
 	SlangLib slang('S', 46257, "acad.kutztown.edu");
